8d6e53c33447474f97a36d00a7000af9a0893761
FIN443 <70796031+FIN443@users.noreply.github.com>
2022-03-09 16:28:49 +0900

    Tab Navigation

--- App.js
+++ App.js
@@ -3,7 +3,9 @@ import React, { useState } from "react";
 import * as Font from "expo-font";
 import { Image, Text } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import { Asset, useAssets } from "expo-asset";
+import { Asset } from "expo-asset";
+import { NavigationContainer } from "@react-navigation/native";
+import Tabs from "./navigation/Tabs";
 
 const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
 const loadImages = (images) =>
@@ -16,10 +18,25 @@ const loadImages = (images) =>
   });
 
 export default function App() {
-  const [assets] = useAssets([require("./profile.jpeg")]);
-  const [loaded] = Font.useFonts(Ionicons.font);
-  if (!assets || !loaded) {
-    return <AppLoading />;
+  const [ready, setReady] = useState(false);
+  const onFinish = () => setReady(true);
+  const startLoading = async () => {
+    // App loading screen
+    const fonts = loadFonts([Ionicons.font]);
+    await Promise.all([...fonts]);
+  };
+  if (!ready) {
+    return (
+      <AppLoading
+        startAsync={startLoading}
+        onFinish={onFinish}
+        onError={console.error}
+      />
+    );
   }
-  return <Text>We are done loading!</Text>;
+  return (
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
+  );
 }
