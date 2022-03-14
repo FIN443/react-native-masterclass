6498e4ecc09f5ab9fe62769eadcb96cfd90b7959
FIN443 <70796031+FIN443@users.noreply.github.com>
2022-03-14 20:47:16 +0900

    Tab Bar Icons

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -5,6 +5,7 @@ import Tv from "../screens/Tv";
 import Search from "../screens/Search";
 import { useColorScheme } from "react-native";
 import colors from "../colors";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
@@ -24,11 +25,40 @@ const Tabs = () => {
         headerTitleStyle: {
           color: isDark ? "white" : colors.black,
         },
+        tabBarLabelStyle: {
+          marginTop: -5,
+          fontSize: 12,
+          fontWeight: "500",
+        },
       }}
     >
-      <Tab.Screen name="Movies" component={Movies} />
-      <Tab.Screen name="Tv" component={Tv} />
-      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen
+        name="Movies"
+        component={Movies}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name={"film-outline"} color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="TV"
+        component={Tv}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name={"tv-outline"} color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name={"search-outline"} color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
