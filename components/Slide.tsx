c7b30b34c8dece551ee994e9eb47218488cdadba
FIN443 <70796031+FIN443@users.noreply.github.com>
2022-03-19 15:13:15 +0900

    Refactor Components

---
+++ components/Slide.tsx
@@ -0,0 +1,77 @@
+import { BlurView } from "@react-native-community/blur";
+import React from "react";
+import { StyleSheet, useColorScheme, View } from "react-native";
+import styled from "styled-components/native";
+import { makeImgPath } from "../utils";
+import Poster from "./Poster";
+
+const BgImg = styled.Image``;
+
+const Title = styled.Text<{ isDark: boolean }>`
+  font-size: 16px;
+  font-weight: 600;
+  color: ${(props) => (props.isDark ? "white" : props.theme.textColor)};
+`;
+const Wrapper = styled.View`
+  flex-direction: row;
+  height: 100%;
+  width: 90%;
+  margin: 0 auto;
+  justify-content: space-around;
+  align-items: center;
+`;
+const Column = styled.View`
+  width: 60%;
+`;
+const OverView = styled.Text<{ isDark: boolean }>`
+  margin-top: 10px;
+  color: ${(props) =>
+    props.isDark ? "rgba(255, 255, 255, 0.8)" : "rgba(0, 0, 0, 0.8)"};
+`;
+const Votes = styled(OverView)`
+  font-size: 12px;
+`;
+
+interface SlideProps {
+  backdropPath: string;
+  posterPath: string;
+  originalTitle: string;
+  voteAverage: number;
+  overview: string;
+}
+
+const Slide: React.FC<SlideProps> = ({
+  backdropPath,
+  posterPath,
+  originalTitle,
+  voteAverage,
+  overview,
+}) => {
+  const isDark = useColorScheme() === "dark";
+  return (
+    <View style={{ flex: 1 }}>
+      <BgImg
+        style={StyleSheet.absoluteFill}
+        source={{ uri: makeImgPath(backdropPath) }}
+      />
+      <BlurView
+        style={StyleSheet.absoluteFill}
+        blurType={isDark ? "dark" : "light"}
+        blurAmount={10}
+        reducedTransparencyFallbackColor={isDark ? "black" : "white"}
+      />
+      <Wrapper>
+        <Poster path={posterPath} />
+        <Column>
+          <Title isDark={isDark}>{originalTitle}</Title>
+          {voteAverage > 0 ? (
+            <Votes isDark={isDark}>⭐{voteAverage}/10</Votes>
+          ) : null}
+          <OverView isDark={isDark}>{overview.slice(0, 100)}...</OverView>
+        </Column>
+      </Wrapper>
+    </View>
+  );
+};
+
+export default Slide;
--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -6,14 +6,11 @@ import Swiper from "react-native-swiper";
 import { BlurView } from "@react-native-community/blur";
 import { makeImgPath } from "../utils";
 import { useColorScheme } from "react-native";
+import Slide from "../components/Slide";
 
 const API_KEY = "96267eb5d8531873d025017bde38767c";
 
 const Container = styled.ScrollView``;
-
-const View = styled.View`
-  flex: 1;
-`;
 
 const Loader = styled.View`
   flex: 1;
@@ -21,44 +18,29 @@ const Loader = styled.View`
   align-items: center;
 `;
 
-const BgImg = styled.Image``;
-
-const Poster = styled.Image`
-  width: 100px;
-  height: 160px;
-  border-radius: 5px;
-`;
-
-const Title = styled.Text`
-  font-size: 16px;
-  font-weight: 600;
-  color: white;
-`;
-const Wrapper = styled.View`
-  flex-direction: row;
-  height: 100%;
-  justify-content: center;
-  align-items: center;
-`;
-const Column = styled.View`
-  width: 40%;
-  margin-left: 15px;
-`;
-const OverView = styled.Text`
-  margin-top: 10px;
-  color: rgba(255, 255, 255, 0.7);
-`;
-const Votes = styled(OverView)`
-  margin-top: 5px;
-  font-size: 12px;
-`;
-
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
-  const isDark = useColorScheme() === "dark";
   const [loading, setLoading] = useState(true);
   const [nowPlaying, setNowPlaying] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
+  const [trending, setTrending] = useState([]);
+  const getTrending = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/trending/movie/week/?api_key=${API_KEY}`
+      )
+    ).json();
+    setTrending(results);
+  };
+  const getUpcoming = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+      )
+    ).json();
+    setUpcoming(results);
+  };
   const getNowPlaying = async () => {
     const { results } = await (
       await fetch(
@@ -66,10 +48,14 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
       )
     ).json();
     setNowPlaying(results);
+  };
+  const getData = async () => {
+    // wait for all data
+    await Promise.all([getTrending(), getUpcoming(), getNowPlaying()]);
     setLoading(false);
   };
   useEffect(() => {
-    getNowPlaying();
+    getData();
   }, []);
   return loading ? (
     <Loader>
@@ -87,28 +73,14 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
         containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
       >
         {nowPlaying.map((movie) => (
-          <View key={movie.id}>
-            <BgImg
-              style={StyleSheet.absoluteFill}
-              source={{ uri: makeImgPath(movie.backdrop_path) }}
-            />
-            <BlurView
-              style={StyleSheet.absoluteFill}
-              blurType={isDark ? "dark" : "light"}
-              blurAmount={10}
-              reducedTransparencyFallbackColor={isDark ? "black" : "white"}
-            />
-            <Wrapper>
-              <Poster source={{ uri: makeImgPath(movie.poster_path) }} />
-              <Column>
-                <Title>{movie.original_title}</Title>
-                {movie.vote_average > 0 ? (
-                  <Votes>⭐{movie.vote_average}/10</Votes>
-                ) : null}
-                <OverView>{movie.overview.slice(0, 100)}...</OverView>
-              </Column>
-            </Wrapper>
-          </View>
+          <Slide
+            key={movie.id}
+            backdropPath={movie.backdropPath}
+            posterPath={movie.poster_path}
+            originalTitle={movie.original_title}
+            voteAverage={movie.vote_average}
+            overview={movie.overview}
+          />
         ))}
       </Swiper>
     </Container>
