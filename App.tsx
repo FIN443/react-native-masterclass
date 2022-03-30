bf0833279656cf94088040a7058bee32372dbd2a
FIN443 <70796031+FIN443@users.noreply.github.com>
2022-03-30 14:18:43 +0900

    Animation State

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Animated, TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 
@@ -16,13 +16,14 @@ const Box = styled.View`
 const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
-  const Y = new Animated.Value(0);
+  const [up, setUp] = useState(false);
+  const Y = useRef(new Animated.Value(0)).current;
+  const toggleUp = () => setUp((prev) => !prev);
   const moveUp = () => {
-    Animated.spring(Y, {
-      toValue: -200,
-      bounciness: 50,
+    Animated.timing(Y, {
+      toValue: up ? 200 : -200,
       useNativeDriver: true,
-    }).start();
+    }).start(toggleUp);
   };
   Y.addListener(() => console.log(Y));
   return (
